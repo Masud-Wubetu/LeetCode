f9282e1c1031b992820d41016242fe76bd70887d
Masud Wubetu <wubetumasud@gmail.com>
2026-03-02 22:34:46 +0300

    Time: 11 ms (90.87%), Space: 19.6 MB (86.53%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        
+        for char in ransom_count:
+            if ransom_count[char] > magazine_count[char]:
+                return False
+        
+        return True
