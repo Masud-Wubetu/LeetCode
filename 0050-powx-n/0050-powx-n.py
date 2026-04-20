eefb9f80ee3261c8aa65d793e883e31fad15dc1c
Masud Wubetu <wubetumasud@gmail.com>
2026-04-20 21:28:17 +0300

    Time: 0 ms (100%), Space: 19.5 MB (60.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,31 +1,16 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        #getting power of x to absolute value of n
-        result = self.myPositivePow(x, abs(n))
-        
-        #if n is negative reverse the result
-        if(n < 0):
-            result = 1 / result
-
-        return result
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        total = 1
+        while n > 0:
+            if n % 2 == 1:
+                total *= x
+            x *= x
+            n //= 2
+        return total
     
-    def myPositivePow(self, x, n):
-        #any number raised to 0 is 1.0
-        if(n == 0):
-            return 1.0
-        
-        #do the half computation
-        halfPower = self.myPositivePow(x, n // 2)
-        fullPower = halfPower * halfPower
 
-        #if the power is odd, multiply fullPower by x
-        if(n % 2 != 0):
-            fullPower *= x
-
-        return fullPower
-        
