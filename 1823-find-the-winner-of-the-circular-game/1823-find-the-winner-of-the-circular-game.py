a3fb9734dcd31cead8fa8041ba47c1f285aff0ad
Masud Wubetu <wubetumasud@gmail.com>
2026-05-04 13:32:53 +0300

    Time: 98 ms (20.14%), Space: 19.1 MB (88.38%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,8 +1,16 @@
+from collections import deque
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        res = 0
+      
+        queue = deque(range(1, n + 1))
 
-        for people in range(1, n + 1):
-            res = (res + k) % people
-        return res + 1
+        while len(queue) > 1:
+            for i in range(k - 1):
+                queue.append(queue.popleft())
+            queue.popleft()
         
+        return queue[0]
+
+
+                    
+            
