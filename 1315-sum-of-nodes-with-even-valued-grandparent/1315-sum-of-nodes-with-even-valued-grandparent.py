9e6bd2bf30526107df5d396df8d3830676e8d96a
Masud Wubetu <wubetumasud@gmail.com>
2026-05-05 09:12:16 +0300

    Time: 7 ms (72%), Space: 22.1 MB (86.67%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
+        
+        def traverse(node, parent, grandParent):
+
+            if not node:
+                return 0
+
+            total = 0
+
+            if grandParent and grandParent.val % 2 == 0:
+                total += node.val
+
+            
+            total += traverse(node.left, node, parent)
+            total += traverse(node.right, node, parent)
+
+            return total
+
+        return traverse(root, None, None)
